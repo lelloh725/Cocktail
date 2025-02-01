7cc6e7bbbe347cf552818a275cc3205752452828
lelloh725 <emanuele.lampis@alice.it>
2025-02-02 00:16:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import sqlite3
 import os
 from flask import Flask, request, jsonify, render_template
 
-app = Flask(__name__)
-CORS(app)  # Abilita CORS per tutte le rotte
-
 from flask_cors import CORS
 CORS(app, resources={r"api/*": {"origins": "*"}})
+
+app = Flask(__name__)
+CORS(app)  # Abilita CORS per tutte le rotte
 
 #app = Flask(__name__)
 #CORS(app)  # Abilita CORS per tutte le rotte
